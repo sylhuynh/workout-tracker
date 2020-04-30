86dc77382c0c00aa92643ca697bb97a550eec774
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-04-29 21:03:50 -0700

    import mongoose and created mongodb connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 
 // TODO: import mongoose
+const mongoose = require("mongoose");
 
 const apiRoutes = require("./routes/api");
 
@@ -18,6 +19,11 @@ app.use(express.json());
 app.use(express.static("public", { "extensions": "html" }));
 
 // TODO: create mongodb connection with mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.use(apiRoutes);
 
